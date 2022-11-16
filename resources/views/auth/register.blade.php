78c5e1d2ac117b7002b97d027dffbd10253c1ebf
Everton <everton.rodrigues1104@gmail.com>
2022-11-16 17:24:23 -0300

    teste recaptcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,11 +8,12 @@
                     <div class="card-header">{{ __('Registrar-se') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
+                        <form method="POST" action="{{ route('register') }}" id="formRegister">
                             @csrf
 
                             <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Nome') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text"
@@ -71,14 +72,14 @@
                                 </div>
                             </div>
 
-                            <div class="g-recaptcha" id="recap" data-sitekey="6LeLGw0jAAAAADuJBosBQG6WPtVWQ9HZ6C92JjlR">
+                            <div class="g-recaptcha" id="recap" data-sitekey="{{ env('RECAPTCHA_KEY') }}">
                             </div>
 
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <a href="#" class="btn btn-primary" onclick="enviarForm()">
                                         {{ __('Registrar') }}
-                                    </button>
+                                    </a>
                                 </div>
                             </div>
                         </form>
@@ -90,32 +91,14 @@
 
     <script>
         function enviarForm() {
-            var forms = document.getElementsByClassName('needs-validation');
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                if (form.checkValidity() === false) {
-                    form.classList.add('was-validated');
-                } else {
-                    if (validarCampos()) {
-                        if (window.location.origin != 'http://127.0.0.1:8000') {
-                            retorno = grecaptcha.getResponse();
-                        } else {
-                            retorno = true;
-                        }
-                        if (retorno) {
-                            form.submit();
-                        } else {
-                            alert('Marque a opção "Eu não sou um robô!".')
-                        }
-                    }
-                }
-            });
+            retorno = grecaptcha.getResponse();
+            if (retorno) {
+                $('#formRegister').submit();
+            } else {
+                alert('Marque a opção "Eu não sou um robô!".')
+            }
         }
     </script>
-
-
-
-    {{-- 6LeLGw0jAAAAADuJBosBQG6WPtVWQ9HZ6C92JjlR      KEY
-        6LeLGw0jAAAAAEzt2E5ejQj4oU48Ri1PyHj142f-      SECRET --}}
 @endsection
 @section('scriptTop')
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
