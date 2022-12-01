ceb7a767c90eec16cc5e286927e1cb32bf650798
everton-adj <everton.rodrigues@adjundiai.org.br>
2022-12-01 16:00:02 -0300

    fix table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
                             <th scope="col"></th>
                             <th scope="col">MEIA</th>
                             <th scope="col">INTEIRA</th>
+                            <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
